32777343dfe96b77eafd720fb5ca92acbd4ab8d6
Aong Cho <aongchouser@gmail.com>
2025-06-15 12:58:07 +0000

    === Deploying to 'yourcar-d9339'...
    i  deploying hosting
    i  hosting[you

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  productionBrowserSourceMaps: false, // Disable source maps for production
   typescript: {
     ignoreBuildErrors: true,
   },
